5be5741e2e29dadfb8366cc5e5e875384eebf07a
dwiel <zdwiel@gmail.com>
2010-03-29 14:34:54 -0400

    first attempt at buildable package

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
-try:
-    from ez_setup import use_setuptools
-    use_setuptools()
-except ImportError:
-    pass
-
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
-    name="dutils",
+    name="threadpool",
     version="0.0.1",
-    description="Useful django utilities",
-    long_description="""This is a boxy package...""",
-    author="Amit Upadhyay",
-    author_email="code@amitu.com",
-    url="http://github.com/amitu/dutils",
-    packages=["dutils"],
+    description="A simple threadpool",
+    author="Zach Dwiel",
+    author_email="zdwiel@gmail.com",
+    url="http://github.com/dwiel/threadpool",
+    packages=["threadpool"],
 )
